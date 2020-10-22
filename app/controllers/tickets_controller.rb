c80836ee3f57f0fd59ec2b3da8697f5115193a2f
sagawashunsuke <shunkurin73@gmail.com>
2020-10-22 10:53:17 +0900

    ユーザー管理機能実装

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -3,5 +3,21 @@ class TicketsController < ApplicationController
   def index
     @tickets = Ticket.all
   end
+
+  def new
+    @ticket = Ticket.new
+  end
+
+  def create
+    Ticket.create(ticket_params)
+  end
+
+
+  private
+
+  def ticket_params
+    params.require(:ticket).permit(:name).merge(user_id: current_user.id)
+  end
+
   
 end
