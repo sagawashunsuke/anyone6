4ec7a3cfd41da23c82ffdc8c8bac8e1aeac2e098
sagawashunsuke <shunkurin73@gmail.com>
2020-10-23 15:21:24 +0900

    購入機能調整

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -20,6 +20,15 @@ class TicketsController < ApplicationController
     end
   end
 
+  def new_guest
+    user = User.find_or_create_by(email: 'guest@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+      user.name = "ゲスト"
+    end
+    sign_in user
+    redirect_to root_path
+  end
+
 
   private
 
